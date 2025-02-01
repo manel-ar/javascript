228097916d39300a560b2725a2a00848914253da
manel-ar <manelarar931@gmail.com>
2025-02-01 12:35:38 +0100

    modif

---
+++ asynAwait.js
@@ -0,0 +1,34 @@
+const baseurl = 'https://jsonplaceholder.typicode.com/'
+// const getusers = () => {
+//     fetch(`${baseurl}/users`)
+//         .then(
+//             (response) => {
+//                 return response.json()
+//             }
+//         )
+//         .then(
+//             (data) => {
+//                 console.log(data)
+//             }
+//         )
+
+//         .catch(error =>
+//             console.log(error));
+
+
+// }
+// getusers()
+const getusers = async () => {
+    try {
+
+
+        const respons = await fetch(`${baseurl}/users`)
+        const data = await respons.json()
+        console.log(data)
+    } catch {
+        error => console.log(error)
+    }
+
+}
+getusers()
+// recuperer les commentaires avecc async await 
